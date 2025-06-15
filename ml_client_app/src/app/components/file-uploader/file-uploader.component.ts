9587e4114793343c51260feb9c6eedba00b68aad
ImedZeiri <imedzairi1996@gmail.com>
2025-06-15 16:20:52 +0100

    reformater la structure

--- ml_client_app/src/app/components/file-uploader/file-uploader.component.ts
+++ ml_client_app/src/app/components/file-uploader/file-uploader.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { InvoiceService } from '../../services/invoice.service';
 import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
@@ -8,17 +8,31 @@ import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
   templateUrl: './file-uploader.component.html',
   styleUrls: ['./file-uploader.component.scss']
 })
-export class FileUploaderComponent {
+export class FileUploaderComponent implements OnChanges {
   @Output() extractionSuccess = new EventEmitter<any>();
+  @Input() extractionData: any = null;
   
   selectedFile: File | null = null;
   isUploading = false;
   dragOver = false;
+  structuredData: any = null;
 
   constructor(
     private invoiceService: InvoiceService,
     private dialog: MatDialog
   ) { }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['extractionData'] && this.extractionData) {
+      this.updateStructuredData();
+    }
+  }
+  
+  updateStructuredData(): void {
+    if (this.extractionData?.invoice?.extracted_content?.structured_data) {
+      this.structuredData = this.extractionData.invoice.extracted_content.structured_data;
+    }
+  }
 
   onFileSelected(event: any): void {
     if (event.target.files) {
@@ -35,6 +49,7 @@ export class FileUploaderComponent {
     }
 
     this.isUploading = true;
+    this.structuredData = null;
 
     this.invoiceService.uploadInvoice(this.selectedFile).subscribe({
       next: (response) => {
